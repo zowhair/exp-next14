74f30f23f38280a590b613d61c13adee32419fdb
root <root@vps.server.local>
2024-04-02 02:20:23 +0000

    fixes

--- src/app/component/Aboutus.tsx
+++ src/app/component/Aboutus.tsx
@@ -37,7 +37,7 @@ export default function AboutUs() {
             body: `Email: ${email} \n  Phone Number: ${phoneNumber} \n Message: ${message}`
         };
 
-        fetch('http://expeditionasiaa.com/api', {
+        fetch('https://expeditionasiaa.com/api', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
